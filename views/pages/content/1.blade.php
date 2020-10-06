f3bdaf713dba6c70c79280fda3c4386eb534a7de
JCEC007 <jcec007@gmail.com>
2020-10-05 20:41:11 -0500

    +add: added the first third pages seeded, Home, History and Contact

---
+++ views/pages/content/1.blade.php
@@ -0,0 +1,21 @@
+<div class="home" data-icontenttype="page" data-icontentid="1">
+
+    <div class="icontenteditable">
+        Home Page
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ views/pages/content/2.blade.php
@@ -0,0 +1,21 @@
+<div class="home" data-icontenttype="page" data-icontentid="2">
+
+    <div class="icontenteditable">
+        Our History
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
