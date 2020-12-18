a248cb36e9faada760e691fd9a4d80c184e4bf59
Juan Escalante <jcec007@gmail.com>
2020-12-17 20:35:06 -0500

    +Add: parent-fit plugin of lazyload for a better image load

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ import Vue from 'vue';
 import axios from 'axios';
 import toastr from 'toastr';
 import 'lazysizes';
-
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 window.bus = new Vue();
 
 /**
